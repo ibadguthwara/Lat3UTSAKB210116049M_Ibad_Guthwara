123a0acb07f9719cbe8e1444815c8a850bae721f
M Ibad Guthwara <43487906+ibadguthwara@users.noreply.github.com>
2019-05-09 15:03:42 +0700

    Initial commit

---
+++ app/src/main/java/id/ibadguthwara/tugas/lat3uts_akb2_10116049_m_ibad_guthwara/MainActivity.java
@@ -0,0 +1,13 @@
+package id.ibadguthwara.tugas.lat3uts_akb2_10116049_m_ibad_guthwara;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/id/ibadguthwara/tugas/lat3uts_akb2_10116049_m_ibad_guthwara/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package id.ibadguthwara.tugas.lat3uts_akb2_10116049_m_ibad_guthwara;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
